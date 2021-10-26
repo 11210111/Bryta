d63edd5ba1c2a496d35bc0e4920382ddf598a66d
11210111 <jagarmj@gmail.com>
2021-10-26 20:49:14 +0900

    add mainpage, nav,movie,actor components layout

--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -1,6 +1,31 @@
+import { Link } from "react-router-dom";
 import "../css/Nav.css";
 function Nav() {
-  return <nav className="nav"></nav>;
+  return (
+    <header>
+      <nav className="nav">
+        <Link to="/">
+          <div className="nav-logo">
+            <img className="logo-img" alt="img" />
+            <span className="logo-text">Bryta</span>
+          </div>
+        </Link>
+        <div className="nav-search">
+          <input type="text" placeholder="배우를 검색해주세요." />
+          <button className="nav-search-icon">🔍</button>
+          {/*임시돋보기 .. 배우상세페이지로 이동*/}
+        </div>
+        <div className="nav-right">
+          <Link to="/mypage">
+            <div className="nav-mypage">mypage</div>
+          </Link>
+          <Link to="/login">
+            <div className="login">login</div>
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 export default Nav;
--- client/src/pages/mainPage.js
+++ client/src/pages/mainPage.js
@@ -1,5 +1,30 @@
+import "../css/MainPage.css";
+import Actor from "../components/Actor";
+import Movie from "../components/Movie";
 function MainPage() {
-  return <div></div>;
+  return (
+    <div id="main-container">
+      <nav className="main-nav">
+        <section className="todayactor">
+          <div className="todayactor-text">오늘의 배우</div>
+          <div className="todayactor-images">
+            <div className="todayactor-actor">
+              <Actor />
+            </div>
+            <div className="todayactor-movies">
+              <Movie />
+              <Movie />
+              <Movie />
+            </div>
+          </div>
+        </section>
+        <section className="main-bryta">
+          <div className="main-bryta-name">Bryta란</div>
+          <p className="main-bryta-about">필모깨기를 도와주는 Bryta</p>
+        </section>
+      </nav>
+    </div>
+  );
 }
 
 export default MainPage;
