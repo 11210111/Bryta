7477e7bc9c251d6048c147b529204f9202b2fb82
ehdgusdl9177 <k23129177@gmail.com>
2021-10-26 18:33:26 +0900

    add logout

--- server/controllers/auth/logout.js
+++ server/controllers/auth/logout.js
@@ -3,7 +3,7 @@ const { deleteRefreshToken, isAuthorized } = require("./token");
 module.exports = (req, res) => {
   const tokenData = isAuthorized(req);
   if (!tokenData) {
-    res.sendStatus(404);
+    res.sendStatus(401);
   }
   deleteRefreshToken(res);
 };
