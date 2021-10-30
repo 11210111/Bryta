a1dbf0c24595146d4f145fa433d6ec9a5789e167
11210111 <jagarmj@gmail.com>
2021-10-31 05:17:51 +0900

    update Nav component

---
+++ client/src/components/LoginErrorModal.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../css/LoginErrorModal.css";
+
+function LoginErrorModal({ modal, setModal }) {
+  return (
+    <>
+      {modal ? (
+        <section className="login-error-modal-section">
+          <div className="login-error-modal-container">
+            <p className="login-error-modal-text">로그인을 해주세요!</p>
+            <Link to="/login">
+              <button
+                className="login-error-modal-btn"
+                onClick={() => setModal(!modal)}
+              >
+                로그인하기
+              </button>
+            </Link>
+            <button
+              className="login-error-modal-btn"
+              onClick={() => setModal(!modal)}
+            >
+              확인
+            </button>
+          </div>
+        </section>
+      ) : null}
+    </>
+  );
+}
+
+export default LoginErrorModal;
