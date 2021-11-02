60cfd565a992a3e1239a18c5468c3bec91b23703
11210111 <jagarmj@gmail.com>
2021-11-02 14:14:31 +0900

    add Mypage

--- client/src/components/Actor.js
+++ client/src/components/Actor.js
@@ -1,24 +1,36 @@
 import "../css/Actor.css";
 import { useState } from "react";
 import LoginErrorModal from "./LoginErrorModal";
-export default function Actor({ actor, isLogin }) {
+import { useSelector } from "react-redux";
+
+export default function Actor({ actor }) {
+  const isLogin = useSelector((state) => state.user);
   const [modal, setModal] = useState(false);
   const modalHandler = () => {
     setModal(!modal);
   };
+  const url = window.location.pathname;
+
   return (
     <div id="actor-container">
-      <img
-        src={actor && actor?.image}
-        alt={actor && actor?.actorName}
-        className="actor-image"
-      />
+      <div className={url === "/mypage" ? "actorimg-crop" : ""}>
+        <img
+          src={actor && actor?.image}
+          alt={actor && actor?.actorName}
+          className={url === "/mypage" ? "favActor-image" : "actor-image"}
+        />
+      </div>
       <div className="actor-name">{actor && actor?.actorName}</div>
       {isLogin ? (
-        <button className="actor-favbtn">즐겨찾기</button>
+        <button className={url === "/mypage" ? "hide" : "actor-favbtn"}>
+          즐겨찾기
+        </button>
       ) : (
         <>
-          <button className="actor-favbtn" onClick={modalHandler}>
+          <button
+            className={url === "/mypage" ? "hide" : "myfav-btn"}
+            onClick={modalHandler}
+          >
             즐겨찾기
           </button>
           <LoginErrorModal modal={modal} setModal={setModal} />
