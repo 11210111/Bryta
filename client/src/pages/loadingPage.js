b20331226a3c94ee685921203bb69c23a014340a
kimdlzp <kimdlzp@gmail.com>
2021-11-04 13:40:52 +0900

    [client] login page css

--- client/src/pages/loadingPage.js
+++ client/src/pages/loadingPage.js
@@ -2,24 +2,74 @@ import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
   height: 100vh;
-  width: 100%
-  margin-top: 18rem
-  text-align: center  
-  `;
-
-const Text = styled.div`
-  font-weight: 600;
-  font-size: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #000;
 `;
 
-const LoadingPage = () => {
-  return (
-    <Container>
-      <Text>Loading...</Text>
-    </Container>
-  );
-};
-console.log("qw");
+const Box = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  width: 160px;
+  height: 160px;
+`;
 
-export default LoadingPage;
+const Logo = styled.img`
+  width: 27px;
+`;
+
+const Text = styled.span`
+  font-size: 23px;
+  color: #ffaa28;
+  animation: fade 3s infinite linear;
+  @keyframes fade {
+    0% {
+      opacity: 1;
+    }
+    50% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+`;
+
+const Loader = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 160px;
+  height: 160px;
+  animation: rotate 3s infinite linear;
+  border: 2px solid transparent;
+  border-top: 2px solid #ffaa28;
+  border-bottom: 2px solid #ffaa28;
+  border-radius: 100%;
+  @keyframes rotate {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(359deg);
+    }
+  }
+`;
+
+export default () => (
+  <Container>
+    <Box>
+      <Logo src={process.env.PUBLIC_URL + "/icon192.png"} />
+      <Text>LOADING</Text>
+      <Loader />
+    </Box>
+  </Container>
+);
