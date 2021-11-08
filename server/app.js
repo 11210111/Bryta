2e55f597b30d04bd8f23a65d03807998c0273899
ehdgusdl9177 <k23129177@gmail.com>
2021-11-08 13:37:29 +0900

    fix https

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ app.use(helmet());
 app.use(
   cors({
     origin: ["http://localhost:3000"],
-    methods: ["GET", "POST", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "DELETE", "PATCH", "OPTIONS"],
     credentials: true,
   })
 );
