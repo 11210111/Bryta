5d35252bc670f3474646c2d5d54251b8e538ee60
11210111 <jagarmj@gmail.com>
2021-11-11 15:09:25 +0900

    add Error page css

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,5 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../css/Error.css";
 
 export default function Error() {
-  return <div>웹페이지를 찾을 수 없습니다</div>;
+  return (
+    <div className="error-page-container">
+      <h3 className="error-page-message">웹페이지를 찾을 수 없습니다</h3>
+      <Link to="/board">
+        <button className="error-page-btn">메인으로 가기</button>
+      </Link>
+    </div>
+  );
 }
